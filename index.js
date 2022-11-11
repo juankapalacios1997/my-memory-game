d596cd8817120dbad2e2efbc79ce352713afad3e
juan.palacios@sryas.com <jcmaster_97@hotmail.com>
2022-11-10 21:35:53 -0600

    20221110 changes

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ let image4 = "url('./assets/dora.jfif')"
 let image5 = "url('./assets/mrclean.jpg')"
 let image6 = "url('./assets/richi-phelps.jfif')"
 
+let cards = document.querySelectorAll('.card');
+console.log(cards);
+
+
 let oddCards = document.querySelectorAll(".odd");
 let evenCards = document.querySelectorAll(".even");
 
@@ -37,16 +41,60 @@ const randomizeImages = () => {
     oddCards.forEach(card => {
         let removed1 = order1.splice(0, 1).pop();
         card.style.backgroundImage = images[removed1];
+        let newClass1 = removed1.toString();
+        card.parentElement.classList.add(newClass1)
     })
 
     evenCards.forEach(card => {
         let removed2 = order2.splice(0, 1).pop();
         card.style.backgroundImage = images[removed2];
+        let newClass2 = removed2.toString();
+        card.parentElement.classList.add(newClass2)
     })
 }
 
 randomizeImages();
 
-let inactiveCards = document.querySelectorAll("inactive");
+let cardOne;
+let cardTwo;
 
-let activeCards = document.querySelectorAll("active");
+const flipCard = (e) => {
+    let clickedCard = e.target;
+    if (clickedCard !== cardOne) {
+        clickedCard.classList.add("flip");
+        if (!cardOne) {
+            return cardOne = clickedCard;
+        } 
+        cardTwo = clickedCard
+        
+        let cardOneDiv= cardOne.classList[1];
+        let cardTwoDiv = cardTwo.classList[1];
+        matchCards(cardOneDiv, cardTwoDiv);
+    }
+}
+
+const matchCards = (img1, img2) => {
+    console.log(img1, img2)
+    if (img1 == img2) {
+        return console.log("Same Cards")
+    }
+    console.log("Not matched")
+}
+
+cards.forEach(card => {
+    card.addEventListener("click", flipCard)
+})
+
+
+// const unflipCard = (e) => {
+//     let clickedCard = e.target;
+//     clickedCard.classList.remove("flip");
+//     cardOne = clickedCard;
+//     cardTwo = clickedCard;
+//     console.log(cardOne, cardTwo);
+// }
+
+
+// flippedCards.forEach(card => {
+//     card.addEventListener("click", unflipCard)
+// })
